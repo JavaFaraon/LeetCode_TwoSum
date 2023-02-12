b40fe45ecfbd3d6a54780365b40bf7d6025d3cdb
JavaFaraon <ivan@rybakovka.com>
2023-02-12 21:46:01 +0200

    Finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+    }
+
+    private void run() {
+        int [] nums = new int[] {1,8,5,7,3};
+        int target = 9;
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,2 +1,20 @@
-package org.example;public class Solution {
+package org.example;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap= new HashMap<>();
+        int[] result = new int[2];
+        for (int i=0; i<nums.length; i++) {
+            int currentNum = nums[i];
+            if (hashMap.containsKey(target-currentNum)) {
+                result[0] = i;
+                result[1] = hashMap.get(target - currentNum); break;
+            } else {
+                hashMap.put(currentNum, i);
+            }
+        }
+        return result;
+    }
 }
